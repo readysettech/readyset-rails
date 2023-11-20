581112be35b55e9d42ba0cf6adf11152aab1295d
Paul Lemus <paullemus@protonmail.com>
2023-11-19 23:08:20 -0800

    feat: improved #route_to_readyset allowed params

--- lib/readyset/controller_extension.rb
+++ lib/readyset/controller_extension.rb
@@ -1,16 +1,35 @@
-# lib/readyset/controller_extension.rb
-
 module Readyset
+  # Module providing controller extensions for routing ActiveRecord queries to a replica database.
   module ControllerExtension
     extend ActiveSupport::Concern
 
-    # Our railtie will make the following available in controllers
     included do
-      def self.route_to_readyset(*actions)
-        around_action only: actions do |_controller, action|
-          # TODO: The role should be flexible
+      # Routes ActiveRecord queries in specified actions to a replica database.
+      #
+      # This method defines an around_action callback that wraps the given actions. Inside the callback,
+      # it switches the ActiveRecord connection to a replica database, ensuring that all database queries
+      # within the actions are executed against the replica.
+      #
+      # @param actions [Array<Symbol, String>] A list of action names to which this routing applies.
+      # @param options [Hash] Additional options to refine the callback application, such as :only or :except.
+      # @param block [Proc] An optional block that can be used for additional logic around the action execution.
+      #
+      # @example
+      #   route_to_readyset :index, :show, only: [:index, :show] do |controller, action_block|
+      #     # Custom logic can be placed here
+      #   end
+      #
+      def self.route_to_readyset(*actions, **options, &block)
+        # Combine actions and options into a single array
+        callbacks = actions << options
+
+        # Define an around_action with the combined callbacks array and the block
+        around_action(*callbacks) do |_controller, action_block|
+          # TODO: Decouple the role symbol, have it pull from a dev-configurable location.
           ActiveRecord::Base.connected_to(role: :replica_db_role) do
-            action.call # All queries will connect to the replica
+            # Functionally the same as yield, except we're highlighting
+            # that it is action_block being called/yielded.
+            action_block.call # All queries will connect to the replica
           end
         end
       end
--- spec/controller_extension_spec.rb
+++ spec/controller_extension_spec.rb
@@ -3,18 +3,18 @@
 require "rails_helper"
 require "readyset/controller_extension"
 
-# We're only using the controller helpers
 RSpec.describe Readyset::ControllerExtension, type: :controller do
-
-  # Mock controller to test our around_action
+  # Global Set-up
   controller(ActionController::Base) do
+    # NOTE: Not required as it's setup by the ReadySet Railtie
+    # include Readyset::ControllerExtension
 
-    # Passes the :index action to ControllerExtension
-    route_to_readyset :index
+    # Main point-of-interest in our fake controller
+    # This line specifies that these queries will be re-routed
+    route_to_readyset only: [:index, :show]
 
     def index
       @posts = Post.where(active: true)
-      @users = User.all
       render plain: "Index"
     end
 
@@ -22,32 +22,59 @@ RSpec.describe Readyset::ControllerExtension, type: :controller do
       @post = Post.find(params[:id])
       render plain: "Show"
     end
+
+    def create
+      Post.create(params[:post])
+      render plain: "Create"
+    end
   end
 
   before do
+    # Need a way to clean this up
     routes.draw do
       get "index" => "anonymous#index"
       get "show/:id" => "anonymous#show"
+      post "create" => "anonymous#create"
     end
 
+    # Our fake queries
     stub_const("Post", Class.new)
-    stub_const("User", Class.new)
     allow(Post).to receive(:where).and_return([])
-    allow(User).to receive(:all).and_return([])
     allow(Post).to receive(:find).and_return(nil)
+    allow(Post).to receive(:create)
   end
 
   describe "#route_to_readyset" do
-    it "routes queries in the index action to the replica database" do
-      # It's yielding the code within the action,
-      # which would be the queries.
-      expect(ActiveRecord::Base).to receive(:connected_to).with(role: :replica_db_role).and_yield
-      get :index
+
+    # Lacks full coverage of possible #around_action parameters, but gets the point across
+    # TODO: Refactor this spec and the mock controller/queries + params
+    # TODO: Change route_to_readyset params for full coverage
+    # TODO: Test to re-route a single query out of an action
+
+    # Sort of a leftover when it was just a symbol
+    context "when accessing the index action" do
+      it "routes queries to the replica database" do
+        # Make sure it's working within the replica "context"
+        # and it is executing the queries via yield
+        expect(ActiveRecord::Base).to receive(:connected_to).with(role: :replica_db_role).and_yield
+        get :index
+      end
     end
 
-    it "does not route queries in the show action to the replica database" do
-      expect(ActiveRecord::Base).not_to receive(:connected_to).with(role: :replica_db_role)
-      get :show, params: { id: 1 }
+    # Check if the options are passing in correctly
+    context "when accessing the show action with :only option" do
+      it "routes queries to the replica database" do
+        expect(ActiveRecord::Base).to receive(:connected_to).with(role: :replica_db_role).and_yield
+        get :show, params: { id: 1 }
+      end
+    end
+
+    # Ensure that non-specified actions aren't getting re-routed
+    context "when accessing an action not included in :only" do
+      it "does not route queries to the replica database" do
+        expect(ActiveRecord::Base).not_to receive(:connected_to).with(role: :replica_db_role)
+        post :create, params: { post: { title: "New Post" } }
+      end
     end
   end
 end
