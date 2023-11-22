ed44f79d22357a302b1c6aac140c3135d47d7ec1
Paul Lemus <paullemus@protonmail.com>
2023-11-21 18:51:34 -0800

    spec: changed how READYSET_URL env var is set

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -4,16 +4,22 @@
 require 'spec_helper'
 require_relative './../lib/ready_set/configuration.rb'
 
-# TODO: Correctly mock the configuration
 RSpec.describe ReadySet::Configuration do
   let(:config) { described_class.new }
+  # Combustion-dependent url string
+  let(:default_url) { 'sqlite3://:@:/db/combustion_test.sqlite' }
+
+  around do |example|
+    original_env = ENV.to_hash
+    example.run
+    ENV.replace(original_env)
+  end
 
   describe '#initialize' do
+    # NOTE: This is providing excessive coverage,
+    # this may be changed later on as we improve config.
     it 'sets default values' do
-      config = ReadySet::Configuration.new
-
-      expect(config.connection_url).to eq(ENV['READYSET_URL'] ||
-                                          'sqlite3://:@:/db/combustion_test.sqlite')
+      expect(config.connection_url).to eq(ENV['READYSET_URL'] || default_url)
       expect(config.database_selector).to eq({ delay: 2.seconds })
       expect(config.database_resolver).to eq(ReadySet::DefaultResolver)
       expect(config.database_resolver_context).to be_nil
@@ -22,17 +28,19 @@ RSpec.describe ReadySet::Configuration do
 
   describe '#connection_url' do
     context 'when READYSET_URL is set' do
-      before { ENV['READYSET_URL'] = 'custom_url' }
-      after { ENV.delete('READYSET_URL') }
-
       it 'returns the value from the environment variable' do
+        ENV['READYSET_URL'] = 'custom_url'
         expect(config.connection_url).to eq('custom_url')
       end
     end
 
-    context 'when READYSET_URL is not set' do
+    # NOTE: Passes, but misleading as this behavior
+    # isn't defaulting within the config class.
+    # Placeholder for later once the config is tackled.
+    xcontext 'when READYSET_URL is not set' do
       it 'returns the default connection URL' do
-        expect(config.connection_url).to eq('sqlite3://:@:/db/combustion_test.sqlite')
+        ENV['READYSET_URL'] = nil
+        expect(config.connection_url).to eq(default_url)
       end
     end
   end
