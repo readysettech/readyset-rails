02804d6a3435aedff88841acca11d0360c89fa19
Paul Lemus <paullemus@protonmail.com>
2023-11-23 13:29:51 -0800

    feat: added logger module

--- lib/ready_set/logger.rb
+++ lib/ready_set/logger.rb
@@ -6,15 +6,37 @@ module ReadySet
   module Logger
     extend ActiveSupport::Concern
 
-    prepended do
-      # Sets up a tag with dynamic content for SQL comments
-      def self.setup_sql_comment_tag(tag_name, content)
-        tag_proc = -> { content }
-        ActiveRecord::QueryLogs.tags << { tag_name => tag_proc }
-      end
+    # Sets up a tag with dynamic content for SQL comments.
+    # @param tag_name [Symbol] The name of the tag.
+    # @param content [String] The content of the tag.
+    def setup_sql_comment_tag(tag_name, content)
+      return if tag_already_exists?(tag_name)
 
-      def self.remove_query_tag(tag)
-        ActiveRecord::QueryLogs.tags.delete(tag)
+      tag_proc = create_tag_proc(content)
+      ActiveRecord::QueryLogs.tags << { tag_name => tag_proc }
+    end
+
+    # Removes a previously set tag.
+    # @param tag [Symbol] The name of the tag to remove.
+    def remove_query_tag(tag)
+      ActiveRecord::QueryLogs.tags.delete(tag)
+    end
+
+    private
+
+    # Creates a Proc for a tag.
+    # @param content [String] The content to be returned by the Proc.
+    # @return [Proc] A Proc that returns the given content.
+    def create_tag_proc(content)
+      -> { content }
+    end
+
+    # Checks if a tag with the given name already exists.
+    # @param tag_name [Symbol] The name of the tag to check.
+    # @return [Boolean] True if the tag exists, false otherwise.
+    def tag_already_exists?(tag_name)
+      ActiveRecord::QueryLogs.tags.any? do |tag|
+        tag.respond_to?(:has_key?) && tag.has_key?(tag_name)
       end
     end
   end
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -4,16 +4,38 @@ require "spec_helper"
 require_relative "./../lib/ready_set/logger.rb"
 
 RSpec.describe ReadySet::Logger do
-  let(:logger) { Class.new { prepend ReadySet::Logger } }
-  let(:query) { "SELECT * FROM users" }
+  # Create a dummy class that includes the Logger module
+  let(:logger) { Class.new { extend ReadySet::Logger } }
 
-  describe ".setup_sql_comment_tag" do
+  describe "#setup_sql_comment_tag" do
+    before do
+      # Clear the tags to ensure a clean environment for each test
+      ActiveRecord::QueryLogs.tags.clear
+    end
+
     it "sets up a tag for SQL comments" do
+      # Call the method on an instance of the dummy class
       tag_name = :custom
       comment = "source: example"
       logger.setup_sql_comment_tag(tag_name, comment)
 
       expect(ActiveRecord::QueryLogs.tags).to include(hash_including(tag_name => kind_of(Proc)))
+    end
+
+    context "when the tag already exists" do
+      before do
+        tag_name = :custom
+        comment = "source: example"
+        # Write a tag
+        logger.setup_sql_comment_tag(tag_name, comment)
+      end
+      it "does not add the tag again" do
+        tag_name = :custom
+        comment = "source: example"
+
+        repeat_tag = logger.setup_sql_comment_tag(tag_name, comment)
+        expect { repeat_tag }.not_to change { ActiveRecord::QueryLogs.tags.count }
+      end
     end
   end
 end
