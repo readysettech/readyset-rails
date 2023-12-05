ff6ccca85908ee6c216b4ebbe3d1f9ae21844bbc
Paul Lemus <paullemus@protonmail.com>
2023-12-05 00:18:13 -0800

    fix: changed config usage

--- lib/readyset.rb
+++ lib/readyset.rb
@@ -2,7 +2,6 @@
 
 require 'readyset/configuration'
 require 'readyset/controller_extension'
-require 'readyset/default_resolver'
 require 'readyset/middleware'
 require 'readyset/query'
 require 'readyset/railtie' if defined?(Rails::Railtie)
@@ -29,7 +28,7 @@ module Readyset
   # @param [Array<Object>] *sql_array the SQL array to be executed against ReadySet
   # @return [PG::Result]
   def self.raw_query(*sql_array)
-    ActiveRecord::Base.establish_connection(Readyset.configuration.connection_url)
+    ActiveRecord::Base.establish_connection(Readyset::Configuration.configuration.database_url)
     ActiveRecord::Base.connection.execute(ActiveRecord::Base.sanitize_sql_array(sql_array))
   end
 end
