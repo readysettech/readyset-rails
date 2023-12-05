35325fa0286438094abc905dd62a7e46c479496f
Paul Lemus <paullemus@protonmail.com>
2023-12-05 00:16:31 -0800

    spec: remake the config spec

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -2,46 +2,31 @@
 # spec/readyset-rails/configuration_spec.rb
 
 require 'spec_helper'
-require_relative './../lib/readyset/configuration.rb'
+require 'readyset/configuration'
 
 RSpec.describe Readyset::Configuration do
   let(:config) { described_class.new }
-  # Combustion-dependent url string
-  let(:default_url) { 'sqlite3://:@:/db/combustion_test.sqlite' }
 
-  around do |example|
-    original_env = ENV.to_hash
-    example.run
-    ENV.replace(original_env)
-  end
-
-  describe '#initialize' do
-    # NOTE: This is providing excessive coverage,
-    # this may be changed later on as we improve config.
-    it 'sets default values' do
-      expect(config.connection_url).to eq(ENV['READYSET_URL'] || default_url)
-      expect(config.database_selector).to eq({ delay: 2.seconds })
-      expect(config.database_resolver).to eq(Readyset::DefaultResolver)
-      expect(config.database_resolver_context).to be_nil
+  context 'when no database_url is specified' do
+    it 'defaults to dummy url' do
+      default_url = 'postgres://user:password@localhost:5432/readyset'
+      expect(config.database_url).to eq default_url
     end
   end
 
-  describe '#connection_url' do
-    context 'when READYSET_URL is set' do
-      it 'returns the value from the environment variable' do
-        ENV['READYSET_URL'] = 'custom_url'
-        expect(config.connection_url).to eq('custom_url')
-      end
+  context 'when database_url ENV var is specified' do
+    it 'is used instead of dummy url' do
+      ENV['READYSET_URL'] = 'postgres://test:password@localhost:5433/readyset'
+      expect(config.database_url).to eq 'postgres://test:password@localhost:5433/readyset'
     end
+  end
 
-    # NOTE: Passes, but misleading as this behavior
-    # isn't defaulting within the config class.
-    # Placeholder for later once the config is tackled.
-    xcontext 'when READYSET_URL is not set' do
-      it 'returns the default connection URL' do
-        ENV['READYSET_URL'] = nil
-        expect(config.connection_url).to eq(default_url)
-      end
+  context 'when database_url is specified' do
+    it 'is used instead of dummy url' do
+      readyset_url = 'postgres://user_test:password@localhost:5433/readyset'
+      config.database_url = readyset_url
+
+      expect(config.database_url).to eq readyset_url
     end
   end
 end
