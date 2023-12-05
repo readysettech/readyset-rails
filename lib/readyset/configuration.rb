f2cb720126e3577b37a212f844cb28facbf12d70
Paul Lemus <paullemus@protonmail.com>
2023-12-05 00:17:57 -0800

    feat: added new config interface for db_url

--- lib/readyset/configuration.rb
+++ lib/readyset/configuration.rb
@@ -2,38 +2,36 @@
 
 module Readyset
   class Configuration
-    attr_accessor :connection_url, :database_selector, :database_resolver,
-      :database_resolver_context
+    attr_accessor :database_url
 
     def initialize
-      @connection_url = ENV['READYSET_URL'] || default_connection_url
-      @database_selector = { delay: 2.seconds }
-      @database_resolver = Readyset::DefaultResolver
-      @database_resolver_context = nil
+      @database_url = ENV['READYSET_URL'] || default_connection_url
+    end
+
+    def self.current_config
+      configuration.inspect
     end
 
     def default_connection_url
       # Check if Rails is defined and if database configuration is available
-      if defined?(Rails) && Rails.application && Rails.application.config.database_configuration
-        # Fetch the environment-specific configuration
-        config = Rails.application.config.database_configuration[Rails.env]
+      'postgres://user:password@localhost:5432/readyset'
+    end
 
-        # Fetch the 'primary_replica' details if available, otherwise fallback to default database
-        replica_config = config['primary_replica'] || config
+    # @return [Readyset::Configuration] Readyset's current configuration
+    def self.configuration
+      @configuration ||= Configuration.new
+    end
 
-        # Construct the URL based on the configuration
-        adapter = replica_config['adapter']
-        user = replica_config['username']
-        password = replica_config['password']
-        host = replica_config['host']
-        port = replica_config['port']
-        db = replica_config['database']
+    # Set Readyset's configuration
+    # @param config [Readyset::Configuration]
+    class << self
+      attr_writer :configuration
+    end
 
-        "#{adapter}://#{user}:#{password}@#{host}:#{port}/#{db}"
-      else
-        # Fallback dummy URL
-        'postgres://user:password@localhost:5432/readyset'
-      end
+    # Modify Readyset's current configuration
+    # @yieldparam [Readyset::Configuration] config current Readyset config
+    def self.configure
+      yield configuration
     end
   end
 end
