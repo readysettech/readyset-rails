193fe1bd88b80e8ba97b257a96921b9c71f9c61b
Ethan Donowitz <ethan@readyset.io>
2023-12-08 17:29:49 -0500

    fix specs

--- spec/relation_extension_spec.rb
+++ spec/relation_extension_spec.rb
@@ -2,34 +2,36 @@
 
 RSpec.describe Readyset::RelationExtension do
   describe '#create_readyset_cache!' do
-    subject { Cat.where(id: 1).create_readyset_cache! }
+    subject { query.create_readyset_cache! }
 
-    let(:expected_query) { 'SELECT "cats".* FROM "cats" WHERE "cats"."id" = ?' }
+    let(:query_string) { query.to_sql }
+    let(:query) { Cat.where(id: 1) }
 
     before do
-      allow(Readyset).to receive(:create_cache!).with(sql: expected_query)
+      allow(Readyset).to receive(:create_cache!).with(sql: query_string)
       subject
     end
 
     it 'invokes `Readyset.create_cache!` with the parameterized query string that the ' \
         'relation represents' do
-      expect(Readyset).to have_received(:create_cache!).with(sql: expected_query)
+      expect(Readyset).to have_received(:create_cache!).with(sql: query_string)
     end
   end
 
   describe '#drop_readyset_cache!' do
-    subject { Cat.where(id: 1).drop_readyset_cache! }
+    subject { query.drop_readyset_cache! }
 
-    let(:expected_query) { 'SELECT "cats".* FROM "cats" WHERE "cats"."id" = ?' }
+    let(:query_string) { query.to_sql }
+    let(:query) { Cat.where(id: 1) }
 
     before do
-      allow(Readyset).to receive(:drop_cache!).with(sql: expected_query)
+      allow(Readyset).to receive(:drop_cache!).with(sql: query_string)
       subject
     end
 
     it 'invokes `Readyset.drop_cache!` with the parameterized query string that the relation ' \
         'represents' do
-      expect(Readyset).to have_received(:drop_cache!).with(sql: expected_query)
+      expect(Readyset).to have_received(:drop_cache!).with(sql: query_string)
     end
   end
 end
