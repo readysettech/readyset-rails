b1b236268eb2d013917d151bad5d4bf7e22d721b
Paul Lemus <paullemus@protonmail.com>
2023-12-11 11:15:13 -0800

    Revert "treewide: Remove resolver middleware" (#50)
    Reverts readysettech/readyset-rails#46

---
+++ spec/middleware_spec.rb
@@ -0,0 +1,19 @@
+# middleware_spec.rb
+
+require 'spec_helper'
+require_relative './../lib/readyset/middleware'
+
+RSpec.xdescribe Readyset::Middleware do
+  let(:app) { double('App', call: true) }
+  let(:env) { {} }
+  let(:middleware) { Readyset::Middleware.new(app) }
+
+  it 'initializes with an app' do
+    expect(middleware.instance_variable_get(:@app)).to eq(app)
+  end
+
+  it 'calls the app with the environment' do
+    expect(app).to receive(:call).with(env)
+    middleware.call(env)
+  end
+end
