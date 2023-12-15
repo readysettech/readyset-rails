66d656e9e23628a4de7ad702446faf1573ba3c91
Paul Lemus <paullemus@protonmail.com>
2023-12-15 15:30:34 -0800

    refactor: removed @database_url
    The instance variable is used anywhere in the gem. Also added spec for shard.

--- lib/readyset/configuration.rb
+++ lib/readyset/configuration.rb
@@ -1,43 +1,12 @@
 # lib/readyset/configuration.rb
 require 'active_record'
-require 'yaml'
-require 'erb'
 
 module Readyset
   class Configuration
-    attr_accessor :database_url, :shard
+    attr_accessor :shard
 
     def initialize
-      @database_url = fetch_database_url
       @shard = :readyset
-    end
-
-    private
-
-    def fetch_database_url
-      url_from_config || url_from_env || log_connection_error
-    rescue StandardError => e
-      Rails.logger.error("Error loading database configuration: #{e.message}")
-      nil
-    end
-
-    def url_from_config
-      return unless defined?(Rails)
-
-      config = load_database_config
-      url = config[Rails.env]['readyset']
-      if url && url['url']
-        url['url']
-      end
-    end
-
-    def url_from_env
-      ENV['READYSET_URL']
-    end
-
-    def load_database_config
-      config_path = Rails.root.join('config', 'database.yml')
-      YAML.safe_load(ERB.new(File.read(config_path)).result) if File.exist?(config_path)
     end
   end
 end
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -4,56 +4,10 @@ require 'spec_helper'
 require 'readyset/configuration'
 
 RSpec.describe Readyset::Configuration do
-  context 'when database.yml present' do
-    it 'defaults to the url defined in config/database.yml' do
-      # Setup
-      # add env var to test that `database.yml` is the default
-      allow(ENV).to receive(:[]).with('READYSET_URL').and_return('env_db_url')
-      allow(Rails).to receive_message_chain(:root,
-                                            :join).and_return('spec/internal/config/database.yml')
-      allow(File).to receive(:read).and_return('test: readyset: url: "test_db_url"')
-
-      # safe_load is standard practice
-      allow(YAML).to receive(:safe_load).
-        and_return({ 'test' => { 'readyset' => { 'url' => 'test_db_url' } } })
-
-      # Exercise
-      config = described_class.new
-
-      # Verify
-      expect(config.database_url).to eq 'test_db_url'
-    end
-  end
-
-  context 'when database_url ENV var is specified' do
-    it 'is used instead of the url in config/database.yml' do
-      # Setup
-      allow(ENV).to receive(:[]).with('READYSET_URL').and_return('env_db_url')
-
-      # Exercise
-      config = described_class.new
-
-      # Verify
-      expect(config.database_url).to eq 'env_db_url'
-    end
-  end
-
-  context 'when neither config/database.yml nor ENV provides a database_url' do
-    it 'logs an error' do
-      # Setup
-      allow(ENV).to receive(:[]).with('READYSET_URL').and_return(nil)
-      allow(Rails).to receive_message_chain(:root,
-                              :join).
-        and_return('spec/internal/config/database.yml')
-      allow(File).to receive(:exist?).and_return(false)
-      allow(Rails.logger).to receive(:error)
-
-      # Exercise
-      config = described_class.new
-
-      # Verify
-      expect(config.database_url).to be_nil
-      expect(Rails.logger).to have_received(:error).once
+  describe '#initialize' do
+    it 'initializes shard with the symbol :readyset' do
+      config = Readyset::Configuration.new
+      expect(config.shard).to eq(:readyset)
     end
   end
 end
