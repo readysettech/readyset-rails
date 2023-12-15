a3329fd3cf8b8267cfc50d71551d0374ddf72c89
Paul Lemus <paullemus@protonmail.com>
2023-12-15 13:39:40 -0800

    fix: fixed broken mock and spec_helper
    I didn't notice that `#safe_load` wasn't added to the mock for
    configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,25 +1,26 @@
 # frozen_string_literal: true
 
-require 'bundler/setup'
+require "bundler/setup"
 Bundler.setup
 
-require 'combustion'
-require 'factory_bot'
-require 'readyset'
-require_relative 'shared_examples'
-Combustion.initialize! :action_controller, :active_record do
-  config.eager_load = true
-end
+require "combustion"
+require "factory_bot"
+require "readyset"
+require_relative "shared_examples"
 
 # This is a bit of a hack. Combustion doesn't appear to support migrating multiple databases, so we
 # just copy the primary database file to serve as the database for our fake ReadySet instance
-primary_db_file = Rails.configuration.database_configuration['test']['primary']['database']
-readyset_db_file = Rails.configuration.database_configuration['test']['readyset']['database']
+
+Combustion.initialize! :action_controller, :active_record do
+  config.eager_load = true
+end
+primary_db_file = Rails.configuration.database_configuration["test"]["primary"]["database"]
+readyset_db_file = Rails.configuration.database_configuration["test"]["readyset"]["database"]
 FileUtils.cp("spec/internal/#{primary_db_file}", "spec/internal/#{readyset_db_file}")
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
+  config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
