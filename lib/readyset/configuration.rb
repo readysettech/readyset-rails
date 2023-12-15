545b7943c5454e23734073e72404b1970d7307d9
Paul Lemus <paullemus@protonmail.com>
2023-12-15 13:42:01 -0800

    refactor: simplified db_url error handling
    Rescues the line that sets the db_url, instead of inside each of the
    methods that set it.

--- lib/readyset/configuration.rb
+++ lib/readyset/configuration.rb
@@ -16,18 +16,18 @@ module Readyset
 
     def fetch_database_url
       url_from_config || url_from_env || log_connection_error
+    rescue StandardError => e
+      Rails.logger.error("Error loading database configuration: #{e.message}")
+      nil
     end
 
     def url_from_config
       return unless defined?(Rails)
 
-      begin
-        config = load_database_config
-        url = config[Rails.env]['readyset']
-        url['url'] if url && url['url']
-      rescue StandardError => e
-        Rails.logger.error("Error loading database configuration: #{e.message}")
-        nil
+      config = load_database_config
+      url = config[Rails.env]['readyset']
+      if url && url['url']
+        url['url']
       end
     end
 
@@ -37,15 +37,7 @@ module Readyset
 
     def load_database_config
       config_path = Rails.root.join('config', 'database.yml')
-      YAML.load(ERB.new(File.read(config_path)).result) if File.exist?(config_path)
-    rescue StandardError => e
-      Rails.logger.error("Failed to load database configuration: #{e.message}")
-      nil
-    end
-
-    def log_connection_error
-      Rails.logger.error('Failed to set database URL: No valid configuration found in config/database.yml or ENV')
-      nil
+      YAML.safe_load(ERB.new(File.read(config_path)).result) if File.exist?(config_path)
     end
   end
 end
