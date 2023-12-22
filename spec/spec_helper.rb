b39622af1f6ceab2f113ae20649e3434eed5041a
Paul Lemus <paullemus@protonmail.com>
2023-12-21 16:02:46 -0800

    fix: fixed comment placement

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,12 +8,13 @@ require 'factory_bot'
 require 'readyset'
 require_relative 'shared_examples'
 
-# This is a bit of a hack. Combustion doesn't appear to support migrating multiple databases, so we
-# just copy the primary database file to serve as the database for our fake ReadySet instance
-
 Combustion.initialize! :action_controller, :active_record do
   config.eager_load = true
 end
+
+# This is a bit of a hack. Combustion doesn't appear to support migrating multiple databases, so we
+# just copy the primary database file to serve as the database for our fake ReadySet instance
+
 primary_db_file = Rails.configuration.database_configuration['test']['primary']['database']
 readyset_db_file = Rails.configuration.database_configuration['test']['readyset']['database']
 FileUtils.cp("spec/internal/#{primary_db_file}", "spec/internal/#{readyset_db_file}")
