acec36710fcd584b0e0a90e264fc4a8a06e3f006
Paul Lemus <paullemus@protonmail.com>
2023-12-22 02:02:07 -0800

    feat: added railtie for query annotation context
    This loads in the `routing_to_readyset?` context to the query tag logs
    list in the Rails app. Currently having difficulties handling the
    initializer order with the Combustion application running.

--- lib/readyset/railtie.rb
+++ lib/readyset/railtie.rb
@@ -13,5 +13,20 @@ module Readyset
         ActiveRecord::Relation.prepend(Readyset::RelationExtension)
       end
     end
+
+    initializer 'readyset.query_annotator' do |app|
+      ActiveSupport.on_load(:after_intialization) do
+        if Rails.configuration.active_record.query_log_tags_enabled
+          Rails.configuration.active_record.query_log_tags ||= []
+          Rails.configuration.active_record.query_log_tags << {
+            routed_to_readyset?: ->(context) do
+              Readyset::QueryAnnotator.routing_to_readyset?
+            end,
+          }
+        else
+          Rails.logger.warn 'Query log tags are either disabled or unavailable.'
+        end
+      end
+    end
   end
 end
